440e3711e5e115d3018f88df9d948c387e5487f8
Anders Karlsen <karlsenanders@gmail.com>
2013-03-09 13:33:15 +0100

    Skeleton on input with angular

---
+++ resources/public/js/update.js
@@ -0,0 +1,48 @@
+"use strict";
+
+$.urlParam = function(name){
+    var results = new RegExp('[\\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    return results ? results[1] || 0 : 0;
+}
+
+
+function UpdateCtrl($scope,$http) {
+	$scope.talk = {
+		presentationType : "presentation",
+		title: "",
+		abstract: "",
+		language: "no",
+		level: "beginner",
+		outline: "",
+		highlight: "",
+		equipment: "",
+		talkTags: [],
+		expectedAudience: "",
+		speakers: [{
+			speakerName: "",
+			email: "",
+			bio: "",
+			picture: null,
+			zipCode: "",
+			givenId: null,
+			dummyId: null
+		}]
+	};
+
+	var talkid = $.urlParam("talkid");
+
+	if (talkid != 0) {
+		var jsonurl = "talkJson?talkid=" + talkid;
+
+
+		$http({method: 'GET', url: jsonurl}).
+	  		success(function(data, status, headers, config) {
+	  			$scope.talk = data;
+	  		}).
+	  		error(function(data, status, headers, config) {
+			    console.log("some error occured");
+		  	});
+	  	
+	}
+
+}
