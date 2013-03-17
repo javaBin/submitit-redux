7f5661653664093403057739b1368a569b223980
Anders Karlsen <karlsenanders@hotmail.com>
2013-03-17 17:50:29 +0100

    Handling maps

--- test/submitit/test/core.clj
+++ test/submitit/test/core.clj
@@ -5,4 +5,5 @@
 (deftest check-do-to-map
 	(is (= {:a "ax" :b "bx"} (do-to-map {:a "a" :b "b"} (fn [x] (str x "x")))) "Basic")
 	(is (= {:a "ax" :b ["bx" "cx"]} (do-to-map {:a "a" :b ["b" "c"]} (fn [x] (str x "x"))))  "With array")
+	(is (= {:a "ax" :b {:ba "bax" :ca "cax"}} (do-to-map {:a "a" :b {:ba "ba" :ca "ca"}} (fn [x] (str x "x"))))  "With array")
 	)
