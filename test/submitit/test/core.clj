6ff74244c2ff1f8de905ec50c3c387a5bbbf76ce
Anders Karlsen <karlsenanders@hotmail.com>
2013-03-17 18:03:24 +0100

    Maps in vector

--- test/submitit/test/core.clj
+++ test/submitit/test/core.clj
@@ -5,5 +5,6 @@
 (deftest check-do-to-map
 	(is (= {:a "ax" :b "bx"} (do-to-map {:a "a" :b "b"} (fn [x] (str x "x")))) "Basic")
 	(is (= {:a "ax" :b ["bx" "cx"]} (do-to-map {:a "a" :b ["b" "c"]} (fn [x] (str x "x"))))  "With array")
-	(is (= {:a "ax" :b {:ba "bax" :ca "cax"}} (do-to-map {:a "a" :b {:ba "ba" :ca "ca"}} (fn [x] (str x "x"))))  "With array")
+	(is (= {:a "ax" :b {:ba "bax" :ca "cax"}} (do-to-map {:a "a" :b {:ba "ba" :ca "ca"}} (fn [x] (str x "x"))))  "With map")
+	(is (= {:a "ax" :b [{:ba "bax" :ca "cax"} {:ba "2bx" :ca "2cx"}]} (do-to-map {:a "a" :b [{:ba "ba" :ca "ca"} {:ba "2b" :ca "2c"}]} (fn [x] (str x "x"))))  "With vector map")
 	)
